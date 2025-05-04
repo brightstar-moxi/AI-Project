f6f96404211bf056a85f2040a9cde89a3804e61e
unknown <brightstarmoxiz2@gmail.com>
2025-05-04 04:57:58 +0100

    Sign up

--- app/provider.js
+++ app/provider.js
@@ -1,6 +1,17 @@
+import { db } from '@/configs/db';
+import { Users } from '@/configs/schema';
+import {useUser} from '@clerk/nextjs';
+import {eq} from 'drizzle-orm'
 import React from 'react'
 
 function provider({children}) {
+    const {user}=useUser();
+
+    const isNewUser=async()=>{
+
+        const result=await db.select().from(Users)
+        .where(eq(Users.email,user?.primaryEmailAddress?.emailAddress))
+    }
   return (
     <div>{children}</div>
   )
