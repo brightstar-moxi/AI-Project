a6a37af29c222e319a08fb54e7a8ba6f02a97497
unknown <brightstarmoxiz2@gmail.com>
2025-05-04 05:24:08 +0100

    Sign up

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import {Button} from "@/components/ui/button";
 import {UserButton} from "@clerk/nextjs";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
