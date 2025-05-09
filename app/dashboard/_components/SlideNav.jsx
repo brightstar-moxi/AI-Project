f7dd5302a83416804f1e99eaa38e543a28a59340
unknown <brightstarmoxiz2@gmail.com>
2025-05-09 04:15:49 +0100

    dashboard updated

---
+++ app/dashboard/_components/SlideNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SlideNav = () => {
+  return (
+    <div className='w-64 h-screen shodow-md p-5'>SlideNav</div>
+  )
+}
+
+export default SlideNav
--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Header from './_components/Header'
+import SlideNav from './_components/SlideNav'
 
 const DashboardLayout = ({children}) => {
   return (
     <div>
+        <div className='hidden md:block h-screen bg-white fixed mt-[65px] w-64'>
+            <SlideNav/>
+        </div>
         <div>
             <Header/>
-        {children}
-            </div></div>
+            <div className='md:ml-64'>
+            {children}
+            </div>
+            </div>
+            </div>
   )
 }
 
