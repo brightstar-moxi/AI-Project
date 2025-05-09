da0e81a65d446c004cfd4ddd63014ecb087a312a
unknown <brightstarmoxiz2@gmail.com>
2025-05-09 04:29:35 +0100

    dashboard updated

--- app/dashboard/_components/SlideNav.jsx
+++ app/dashboard/_components/SlideNav.jsx
@@ -1,6 +1,41 @@
+
+import { PanelsTopLeft } from 'lucide-react'
 import React from 'react'
 
 const SlideNav = () => {
+
+    const MenOption=[
+        {
+            id:1,
+            name:"Dashboard",
+            path:'/dashboard',
+            icon:PanelsTopLeft
+        },
+        {
+            id:1,
+            name:"Create New",
+            path:'/create-new',
+            icon:PanelsTopLeft
+        },
+        {
+            id:1,
+            name:"Upgrade",
+            path:'/upgrade',
+            icon:PanelsTopLeft
+        },
+        {
+            id:1,
+            name:"Account",
+            path:'/account',
+            icon:PanelsTopLeft
+        },
+        // {
+        //     id:1,
+        //     name:"Dashboard",
+        //     path:'/dashboard',
+        //     icon:PanelsTopLeft
+        // },
+    ]
   return (
     <div className='w-64 h-screen shodow-md p-5'>SlideNav</div>
   )
