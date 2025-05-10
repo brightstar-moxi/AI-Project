3dced4e59b8176fb66e86f4ff3961101425e9e7d
unknown <brightstarmoxiz2@gmail.com>
2025-05-10 18:38:58 +0100

    routing dashboard

--- app/dashboard/create-new/_components/SelectTopic.jsx
+++ app/dashboard/create-new/_components/SelectTopic.jsx
@@ -1,7 +1,23 @@
+"use client"
 import React from 'react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 export const SelectTopic = () => {
   return (
-    <div>SelectTopic</div>
+    <div>
+      <h2 className='font-bold text-2xl text-[#8338ec]'>Content</h2>
+      <p className='text-gray-500'>What is the topic of your video?</p>
+      <Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select>
+
+    </div>
   )
 }
