f48c6d9716aad42360a99ebee8b793c9ab48ee7e
unknown <brightstarmoxiz2@gmail.com>
2025-05-10 05:56:12 +0100

    dashboard updating

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,13 +1,21 @@
+"use client"
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import React, { useState } from 'react'
+import EmptyState from './_components/EmptyState'
 
 const Dashboard = () => {
+  const  [videoList,setVideList]=useState([])
   return (
     <div>
-      <div>
-        <h2>Dashboard</h2>
+      <div className='flex justify-between items-baseline-last'>
+        <h2 className='font-bold text-2xl text-[#8338ec]'>Dashboard</h2>
         <Button className='bg-[#8338ec]'>+ Create New</Button>
       </div>
+
+      {/* Empty State */}
+      {videoList?.length==0&&<div>
+        <EmptyState/>
+        </div>}
     </div>
   )
 }
