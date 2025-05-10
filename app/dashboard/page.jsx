683d219c77ea075e3a697c154d325213239e8f82
unknown <brightstarmoxiz2@gmail.com>
2025-05-10 05:26:40 +0100

    dashboard updating

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <div>
+        <h2>Dashboard</h2>
+        <Button className='bg-[#8338ec]'>+ Create New</Button>
+      </div>
+    </div>
   )
 }
 
