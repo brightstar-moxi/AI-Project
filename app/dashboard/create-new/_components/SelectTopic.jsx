41faab8f60d85925150766ad8079e5c2e8378a54
unknown <brightstarmoxiz2@gmail.com>
2025-05-11 03:23:04 +0100

    creete-new structure for ai

--- app/dashboard/create-new/_components/SelectTopic.jsx
+++ app/dashboard/create-new/_components/SelectTopic.jsx
@@ -1,23 +1,29 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react';
+import {Textarea} from "@/components/ui/textarea"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 export const SelectTopic = () => {
+  const options = ['Custom Prompt', 'Random AI Story', 'Scary Story', 'Historical Facts', 'Bed Time Story', 'Motivaional', 'Fun Facts']
+  const [selectedOption, setSelectedOption] = useState()
   return (
     <div>
       <h2 className='font-bold text-2xl text-[#8338ec]'>Content</h2>
       <p className='text-gray-500'>What is the topic of your video?</p>
-      <Select>
-  <SelectTrigger className="w-[180px]">
-    <SelectValue placeholder="Theme" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="light">Light</SelectItem>
-    <SelectItem value="dark">Dark</SelectItem>
-    <SelectItem value="system">System</SelectItem>
-  </SelectContent>
-</Select>
+      <Select onValueChange={(value) => setSelectedOption(value)}>
+        <SelectTrigger className="w-full mt-2 p-6 text-lg">
+          <SelectValue placeholder="Content Type" />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((item, index) => (
+            <SelectItem value={item}>{item}</SelectItem>
+          ))}
 
+        </SelectContent>
+      </Select>
+      {selectedOption == 'Custom Prompt' &&
+<Textarea/>
+}
     </div>
   )
 }
