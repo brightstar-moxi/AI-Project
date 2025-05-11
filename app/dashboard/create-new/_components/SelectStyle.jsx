d4b7882e09880fc04852327a417b957fa100be2c
unknown <brightstarmoxiz2@gmail.com>
2025-05-11 04:32:24 +0100

    creete-new structure for ai

--- app/dashboard/create-new/_components/SelectStyle.jsx
+++ app/dashboard/create-new/_components/SelectStyle.jsx
@@ -1,34 +1,40 @@
+import Image from 'next/image'
 import React from 'react'
 
 export const SelectStyle = () => {
     const styleOptions=[
         {
             name:'Realstic',
-            image:'real.jpg'
-        },
-
-        {
-            name:'Realstic',
-            image:'real.jpg'
+            image:'/real.jpg'
         },
         {
             name:'Cartoon',
-            image:'cartoon.jpeg'
+            image:'/cartoon.jpeg'
         },
         {
             name:'Comic',
-            image:'comic.png'
+            image:'/comic.png'
         },
         {
             name:'WaterColor',
-            image:'watermark.jpg'
+            image:'/watermark.jpg'
         },
         {
             name:'GTA',
-            image:'gta.jpeg'
+            image:'/gta.jpeg'
         },
     ]
   return (
-    <div>SelectStyle</div>
+    <div>
+        <h2 className='font-bold text-2xl text-[#8338ec]'>Style</h2>
+        <p className='text-gray-500'>Select your video style</p>
+        <div>
+            {styleOptions.map((item, index)=>(
+                <div>
+                    <Image src={item.image} alt='A.I image' width={100} height={100}/> 
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
