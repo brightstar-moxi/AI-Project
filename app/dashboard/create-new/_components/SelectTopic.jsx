411ed6aeb189da37582e569e23778a164698201b
unknown <brightstarmoxiz2@gmail.com>
2025-05-11 03:50:53 +0100

    creete-new structure for ai

--- app/dashboard/create-new/_components/SelectTopic.jsx
+++ app/dashboard/create-new/_components/SelectTopic.jsx
@@ -3,14 +3,17 @@ import React, { useState } from 'react';
 import {Textarea} from "@/components/ui/textarea"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
-export const SelectTopic = () => {
+export const SelectTopic = ({onUserSelect}) => {
   const options = ['Custom Prompt', 'Random AI Story', 'Scary Story', 'Historical Facts', 'Bed Time Story', 'Motivaional', 'Fun Facts']
   const [selectedOption, setSelectedOption] = useState()
   return (
     <div>
       <h2 className='font-bold text-2xl text-[#8338ec]'>Content</h2>
       <p className='text-gray-500'>What is the topic of your video?</p>
-      <Select onValueChange={(value) => setSelectedOption(value)}>
+      <Select onValueChange={(value) => {
+        setSelectedOption(value)
+        value!='Custom Prompt'&&onUserSelect('topic',value)
+        }}>
         <SelectTrigger className="w-full mt-2 p-6 text-lg">
           <SelectValue placeholder="Content Type" />
         </SelectTrigger>
@@ -22,7 +25,9 @@ export const SelectTopic = () => {
         </SelectContent>
       </Select>
       {selectedOption == 'Custom Prompt' &&
-<Textarea/>
+<Textarea className='mt-3' 
+onChange={(e)=>onUserSelect('topic',e.target.value)}
+placeholder='Write prompt on which  you want to generate video'/>
 }
     </div>
   )
--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { SelectTopic } from './_components/SelectTopic'
 
 const CreateNew = () => {
+
+  const [formData,setFormData]=useState([])
+  const onHandleInputChage=(fieldName, fieldValue)=>{
+console.log(fieldName,fieldValue);
+
+  }
   return (
     <div className='md:px-20'>
       <h2 className='font-bold text-4xl text-center text-[#8338ec]'>Create New</h2>
     <div className='mt-10 shadow-md p-10'>
       {/* Select Topic  */}
-<SelectTopic/>
+<SelectTopic onUserSelect={onHandleInputChage}/>
       {/* Select Style */}
 
       {/* Duration */}
